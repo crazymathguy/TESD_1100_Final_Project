01c9b7555b8b60310f9cc8772f588a4e7c0b8545
Sean Briggs <iluv2program@gmail.com>
2025-11-13 10:49:42 -0700

    Code generated cube

---
+++ cube-data.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+    The data for the cube.js script to use
+
+    Author: Sean Briggs
+    Date:   2025-11-13
+
+    Filename: cube-data.js
+
+*/
+
+var colors = ["gray", "white", "yellow", "red", "orange", "green", "blue"];
+var cubeNames = ["White Cross"];
+
+var cubeCaptions = ["The White Cross"];
+
+var cubeData = [[[0,1,0,1,1,1,0,1,0], [0,3,0,0,3,0,0,0,0], [0,6,0,0,6,0,0,0,0]]];
---
+++ cube.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*
+	Rubik's Solver code to display an example cube
+
+	Author: Sean Briggs
+	Date:   2025-11-13
+
+	Filename: cube.js
+
+*/
+
+function createCube(name) {
+	var cubeIndex = cubeNames.findIndex(name);
+	var thisCube = cubeData[cubeIndex];
+	for (var face = 0; face < 3; face++) {
+		var htmlCode = "<table class='face" + (face + 1) + "'> ";
+		for (var i = 0; i < 3; i++) {
+			htmlCode += "<tr> ";
+			for (var j = 0; j < 3; j++) {
+				htmlCode += "<td class='" + colors[thisCube[face][3 * i + j]] + "'></td> ";
+			}
+			htmlCode += "</tr> ";
+		}
+		htmlCode += "</table> ";
+	}
+	htmlCode += "<figcaption>" + cubeCaptions[cubeIndex] + "</figcaption>";
+
+	document.getElementById(name).innerHTML = htmlCode;
+}
